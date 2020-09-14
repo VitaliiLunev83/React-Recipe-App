387b34001325aed2e75391bffcdc39643595e514
Vitalii <vitalii.luniev83@gmail.com>
2020-09-14 17:54:25 +0300

    Updates

---
+++ precache-manifest.f8bff5bcd5a9a8a7e501c4c5f1fb28b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71af6b27990a79ced2fb77d0ba880a72",
+    "url": "/React-Recipe-App/index.html"
+  },
+  {
+    "revision": "62eef1f17497a508a970",
+    "url": "/React-Recipe-App/static/css/main.613ceed0.chunk.css"
+  },
+  {
+    "revision": "ea43401e5c8c9d38df5a",
+    "url": "/React-Recipe-App/static/js/2.e049d543.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Recipe-App/static/js/2.e049d543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62eef1f17497a508a970",
+    "url": "/React-Recipe-App/static/js/main.daf48a23.chunk.js"
+  },
+  {
+    "revision": "05ac211575e4d4bf7ea2",
+    "url": "/React-Recipe-App/static/js/runtime-main.65730133.js"
+  }
+]);
